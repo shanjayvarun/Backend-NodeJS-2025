5248e5b3c5c8d0f90dc9352f1642a136ebc195bd
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-01-31 21:30:05 +0530

    Test Commit

--- folders/controllers/task.controller.js
+++ folders/controllers/task.controller.js
@@ -1,7 +1,7 @@
 const { sendSuccessPost, sendError, sendSuccessGet } = require("../middlewares/response.middleware");
 const taskService = require("../services/task.service");
 
-exports.createTask = async (req, res) => {
+exports.createTask = async (req, res) => {    
     try {
         const task = await taskService.saveTask(req.body);
         return task ? sendSuccessPost(res, task, 'Task created successfully') : sendError(res, res.status(400), 'Failed to create task');
