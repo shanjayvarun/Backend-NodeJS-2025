221643f6221545533e4b661f6c6fcf1d32c37a9b
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-02-07 17:56:51 +0530

    unique true completed

--- folders/models/blog.model.js
+++ folders/models/blog.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     content: { type: String, required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     tags: [{ type: String }],
