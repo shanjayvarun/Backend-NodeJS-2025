4a03dfa308334214520b33e3399b1cd34a7d1d6a
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-02-12 11:18:08 +0530

    log found

--- config/db.config.js
+++ config/db.config.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log('MongoDB connected by SHANJAY VARUN');
   } catch (error) {
     console.error('MongoDB connection failed:', error);
     process.exit(1);
