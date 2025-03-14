85efc2fc6b27418c0fe597091428206dc6dc932c
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-03-14 13:13:56 +0530

    Restricted Routes Completed

--- folders/routes/task.routes.js
+++ folders/routes/task.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const taskController = require('../controllers/task.controller');
 const { validateTaskCreation, validateToken } = require('../middlewares/validation.middleware');
+const { checkRole } = require('../middlewares/roles.middleware');
 
 const router = express.Router();
 
-router.post("/create-task", validateToken, validateTaskCreation, taskController.createTask);
+router.post("/create-task", validateToken, validateTaskCreation, checkRole([ROLES.ADMIN, ROLES.USER]), taskController.createTask);
 router.get("/get-tasks", validateToken, taskController.getTasks);
 router.get("/get-task/:id", validateToken, taskController.getTasks);
 router.patch("/update-task/:id", validateToken, taskController.updateTask);
