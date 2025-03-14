d280825d00474a89f3da73961bfc4603553618fe
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-03-14 15:03:22 +0530

    Bug Fixed

--- folders/x-unused-codes/next-approach.js
+++ folders/x-unused-codes/next-approach.js
@@ -1,6 +1,6 @@
-// 1️⃣ Add JWT Middleware to protect routes (/tasks, /profile, etc.).
 // 2️⃣ Implement Refresh Tokens to avoid JWT expiration issues.
 // 3️⃣ Integrate Rate Limiting (Prevent brute-force attacks).
 // 3️⃣ Improve Logging (Use Winston or Morgan for error logs)
 // 2️⃣ Add Email Verification (Send confirmation emails)?
 // 3️⃣ Create a Forgot Password API?
+// token blacklisting
