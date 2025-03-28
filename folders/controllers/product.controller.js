e95636211f0ea1be6183efdd74b39c51b7124150
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-03-28 18:10:07 +0530

    Search funcyionality completed

--- folders/controllers/product.controller.js
+++ folders/controllers/product.controller.js
@@ -12,12 +12,19 @@ exports.addProduct = async (req, res) => {
 
 exports.getProducts = async (req, res) => {
     try {
-        const products = req.params.id ? await productService.getProductById(req.params.id) : await productService.getProducts(req.query.page, req.query.limit);
-        return products ? sendSuccessGet(res, { [req.params.id ? 'product' : 'products']: products, totalCount: products.length }, 'Products fetched successfully') : sendError(res, 404, 'No Products found');
+        let { category, search, sort, inStock, order, skip, limit } = req.query;
+        let filter = {};
+        let sortQuery = {};
+        if (category) { filter.category = category }
+        if (inStock) { filter.inStock = inStock }
+        if (search) { filter.productName = { $regex: search, $options: "i" } }
+        if (sort) { let sortOrder = order === "desc" ? -1 : 1; sortQuery[sort] = sortOrder; }
+        const products = req.params.id ? await productService.getProductById(req.params.id) : await productService.getProducts(filter, sortQuery, skip, limit);
+        return products ? sendSuccessGet(res, { [req.params.id ? "product" : "products"]: products, totalCount: products.length }, "Products fetched successfully") : sendError(res, 404, "No Products found");
     } catch (error) {
-        return sendError(error, 500, error.message);
+        return sendError(res, 500, error.message);
     }
-}
+};
 
 exports.updateProduct = async (req, res) => {
     try {
