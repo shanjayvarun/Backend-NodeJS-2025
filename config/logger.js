0a7a2a66dc1bf89264af5b7d47b61aa2a24e4548
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-04-01 16:51:52 +0530

    Logs changed

--- config/logger.js
+++ config/logger.js
@@ -16,8 +16,7 @@ const logger = winston.createLogger({
         winston.format.json()
     ),
     transports: [
-        new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
-        new winston.transports.File({ filename: path.join(logDir, 'combined.log') })
+        new winston.transports.File({ filename: path.join(logDir, 'All.log') })
     ]
 });
 
