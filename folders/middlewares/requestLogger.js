43c251202edc8d2bed24e84e97e3fc170f02e3a4
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-04-01 16:44:18 +0530

    request logger changed

--- folders/middlewares/requestLogger.js
+++ folders/middlewares/requestLogger.js
@@ -1,14 +1,23 @@
 const logger = require('../../config/logger');
 
 const requestLogger = (req, res, next) => {
-    const logData = {
-        method: req.method,
-        url: req.originalUrl || req.url,
-        ip: req.ip,
-        query: req.query,
-        body: req.body,
-    };
-    logger.info(`${JSON.stringify(logData)}`);
+    const startTime = process.hrtime();
+    res.on('finish', () => {
+        const diff = process.hrtime(startTime);
+        const responseTime = (diff[0] + diff[1] * 1e-9).toFixed(2);
+        const logData = {
+            method: req.method,
+            url: req.originalUrl || req.url,
+            statusCode: res.statusCode,
+            ip: req.ip,
+            query: req.query,
+            body: req.body,
+            responseTime: `${responseTime} Seconds`,
+            // headers: req.headers,
+            // userAgent: req.get('User-Agent'),
+        };
+        logger.info(`${JSON.stringify(logData, null, 2)}`);
+    });
     next();
 };
 
