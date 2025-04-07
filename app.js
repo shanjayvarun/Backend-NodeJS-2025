8bd3713cc3e89fce0105f8e6e95d089993961620
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-04-07 11:47:33 +0530

    Chat Modules Created

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const taskRoutes = require('./folders/routes/task.routes');
 const blogRoutes = require('./folders/routes/blog.routes');
 const productRoutes = require('./folders/routes/product.routes')
 const utilityRoutes = require('./folders/routes/utility.routes');
+const chatRoutes = require('./folders/routes/chat.routes');
 const requestLogger = require('./folders/middlewares/requestLogger');
 require('./config/passport.config');
 
@@ -25,6 +26,7 @@ app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/tasks`, taskRoutes);
 app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/blogs`, blogRoutes);
 app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/utility`, utilityRoutes);
 app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/products`, productRoutes);
+app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/chats`, chatRoutes);
 //App routes <-- END -->
 
 module.exports = app;
