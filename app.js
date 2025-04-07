f21d7df788e47c8d118cd27176a5dcd6905c52ea
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-04-07 17:35:31 +0530

    ROUTES FIXED

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db.config');
 const passport = require('passport');
 const cors = require("cors");
-const userRoutes = require('./folders/routes/user.routes');
-const taskRoutes = require('./folders/routes/task.routes');
-const blogRoutes = require('./folders/routes/blog.routes');
-const productRoutes = require('./folders/routes/product.routes')
-const utilityRoutes = require('./folders/routes/utility.routes');
-const chatRoutes = require('./folders/routes/chat.routes');
 const requestLogger = require('./folders/middlewares/requestLogger');
+const configureRoutes = require('./config/routes.config');
 require('./config/passport.config');
 
 dotenv.config();
@@ -19,14 +14,6 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(cors())
 app.use(requestLogger);
-
-//App routes <-- START -->
-app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/users`, userRoutes);
-app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/tasks`, taskRoutes);
-app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/blogs`, blogRoutes);
-app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/utility`, utilityRoutes);
-app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/products`, productRoutes);
-// app.use(`/${process.env.SUBDOMAIN}/${process.env.VERSION}/chats`, chatRoutes);
-//App routes <-- END -->
+configureRoutes(app);
 
 module.exports = app;
--- config/routes.config.js
+++ config/routes.config.js
@@ -1,18 +1,18 @@
+const { ROUTE_PREFIX } = require('../folders/utility/enum');
 const userRoutes = require('../folders/routes/user.routes');
 const taskRoutes = require('../folders/routes/task.routes');
 const blogRoutes = require('../folders/routes/blog.routes');
 const productRoutes = require('../folders/routes/product.routes');
 const utilityRoutes = require('../folders/routes/utility.routes');
-const chatRoutes = require('../folders/routes/chat.routes');
-const { ROUTE_PREFIX } = require('../folders/utility/enum');
+// const chatRoutes = require('../folders/routes/chat.routes');
 
 const configureRoutes = (app) => {
-    app.use(`/${ROUTE_PREFIX}/users`, userRoutes);
-    app.use(`/${ROUTE_PREFIX}/tasks`, taskRoutes);
-    app.use(`/${ROUTE_PREFIX}/blogs`, blogRoutes);
-    app.use(`/${ROUTE_PREFIX}/utility`, utilityRoutes);
-    app.use(`/${ROUTE_PREFIX}/products`, productRoutes);
-    // app.use(`/${ROUTE_PREFIX}/chats`, chatRoutes);
-  };
-  
-  module.exports = configureRoutes;
+  app.use(`${ROUTE_PREFIX}/users`, userRoutes);
+  app.use(`${ROUTE_PREFIX}/tasks`, taskRoutes);
+  app.use(`${ROUTE_PREFIX}/blogs`, blogRoutes);
+  app.use(`${ROUTE_PREFIX}/utility`, utilityRoutes);
+  app.use(`${ROUTE_PREFIX}/products`, productRoutes);
+  // app.use(`/${ROUTE_PREFIX}/chats`, chatRoutes);
+};
+
+module.exports = configureRoutes;
