dee72aa90aec1f8f0c3be28c211aa84dc28dec04
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-09-23 15:29:31 +0530

    Testing NodeJS

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 const app = require('./app');
 const http = require("http");
-const socketIo = require("socket.io");
 const PORT = process.env.PORT || 3000;
 const logger = require('./config/logger');
-const initializeSocket = require('./config/socket');
 
 const server = http.createServer(app);
-
-const io = socketIo(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-
-initializeSocket(io);
 
 server.listen(PORT, () => {
   logger.info(`Server started on port ${PORT}`);
