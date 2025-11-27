094b5965b24802c44c8faf3b4413912f9ec54cd0
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-11-27 17:50:42 +0530

    test name removed in PROD

--- folders/controllers/user.controller.js
+++ folders/controllers/user.controller.js
@@ -25,7 +25,6 @@ exports.loginUser = (req, res, next) => {
         name: user.name,
         role: user.role,
         lastLoginAt: user.lastLoginAt,
-        test: 'shanjay'
       }
     }, 'User logged in successfully');
   })(req, res, next);
