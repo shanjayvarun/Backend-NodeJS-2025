9704ded707d1058d709ce57f133ac508af15ab4e
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-11-28 15:29:07 +0530

    test value removed

--- folders/controllers/user.controller.js
+++ folders/controllers/user.controller.js
@@ -25,7 +25,6 @@ exports.loginUser = (req, res, next) => {
         name: user.name,
         role: user.role,
         lastLoginAt: user.lastLoginAt,
-        key: 'value'
       }
     }, 'User logged in successfully');
   })(req, res, next);
