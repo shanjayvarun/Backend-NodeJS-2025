1589c1c0c3a2b280f5f35356f7eb1958105f8b82
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-11-28 15:18:19 +0530

    user test value added

--- folders/controllers/user.controller.js
+++ folders/controllers/user.controller.js
@@ -25,6 +25,7 @@ exports.loginUser = (req, res, next) => {
         name: user.name,
         role: user.role,
         lastLoginAt: user.lastLoginAt,
+        key: 'value'
       }
     }, 'User logged in successfully');
   })(req, res, next);
