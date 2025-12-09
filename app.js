5a32c71a53834a199173d1d54c315818918ad861
shanjay-appiyo <shanjayappiyo@gmail.com>
2025-12-09 14:38:32 +0530

    SLight Vhnag in APP.js Need to change later

--- app.js
+++ app.js
@@ -5,16 +5,39 @@ const cors = require("cors");
 const requestLogger = require('./folders/middlewares/requestLogger');
 const configureRoutes = require('./config/routes.config');
 require('./config/passport.config');
+const logger = require('../CRM-Backend/config/logger');
 
 connectDB();
-
 const app = express();
 
 app.use(express.json());
 app.use(passport.initialize());
-app.use(cors())
+app.use(cors());
 app.use(requestLogger);
 
+app.get("/test-error", (req, res) => {
+  throw new Error("CloudWatch Alarm Test Error");
+});
+
 configureRoutes(app);
+
+// 👇 Global Error Logger - LAST middleware
+app.use((err, req, res, next) => {
+    const logData = {
+        message: err.message,
+        stack: err.stack,
+        method: req.method,
+        url: req.originalUrl,
+        ip: req.ip,
+        headers: req.headers,
+    };
+
+    logger.error(JSON.stringify(logData, null, 2));
+
+    res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+    });
+});
 
 module.exports = app;
