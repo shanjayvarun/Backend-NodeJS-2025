6b91bb2cf07ab69c0a312cd0364c3bfad609fea5
shanjay-appiyo <shanjayappiyo@gmail.com>
2026-04-30 09:23:41 +0530

    overide changed

--- services/auth-service/src/config/env.js
+++ services/auth-service/src/config/env.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const envFile = process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev';
 const projectRoot = path.resolve(__dirname, '../../../..');
 
-dotenv.config({ path: path.resolve(projectRoot, envFile) });
+dotenv.config({ path: path.resolve(projectRoot, envFile), override: true });
 
 const getRequiredEnv = (key) => {
   const value = process.env[key];
