28fdc9fad674a7432c83f35d7b39a5e0c55c0e1b
shanjay-appiyo <shanjayappiyo@gmail.com>
2026-05-06 14:47:21 +0530

    main .tf fiel commited

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,27 +100,27 @@ resource "aws_instance" "crms_server_tf" {
   vpc_security_group_ids = [aws_security_group.crms_sg_tf.id]
   # Runs shell commands during server boot. Like Startup Automation
   user_data = <<EOF
-              # Update Packages
-              dnf update -y
-              # Install Docker + Git
-              dnf install -y docker git
-              # Start Docker
-              systemctl start docker
-              # Auto Start Docker on ReBoot
-              systemctl enable docker
-              # Allows ec2-user to run docker commands.
-              usermod -aG docker ec2-user
-              # Create folder
-              mkdir -p /usr/local/lib/docker/cli-plugins/
-              # Download Docker Compose
-              curl -SL https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-              # Make executable
-              chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-              # Create Project Folder
-              mkdir -p /home/ec2-user/crms-backend
-              # Give Ownership
-              chown ec2-user:ec2-user /home/ec2-user/crms-backend
-              EOF
+#!/bin/bash
+set -e
+# Update packages
+dnf update -y
+# Install Git + Docker
+dnf install -y git docker
+# Start Docker
+systemctl start docker
+systemctl enable docker
+# Add ec2-user to docker group
+usermod -aG docker ec2-user
+# Install Docker Compose
+mkdir -p /usr/local/lib/docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-linux-x86_64 \
+-o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+# Create project folder
+mkdir -p /home/ec2-user/crms-backend
+# Ownership
+chown -R ec2-user:ec2-user /home/ec2-user/crms-backend
+EOF
   # Terraform lifecycle behavior.
   lifecycle {
     # if True Means It Prevents Accidental Deletion False means Deletion Allowed. Very uch Useful for Production DBs
