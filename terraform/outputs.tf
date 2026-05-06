e968d91cce0f16afe2689952cab229f0ca455250
Shanjay Varun <82082315+shanjayvarun@users.noreply.github.com>
2026-05-06 12:49:17 +0530

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Special Terraform block used to expose values after terraform apply.
-output "prod_instance_ip" {
-  description = "The Public IP to put into GitHub Secrets"
-  value       = aws_instance.crms_server_tf.public_ip
-}
