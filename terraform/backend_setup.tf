120cd3cda6b2b87b1cb4526a66856d807e2dcb66
shanjay-appiyo <shanjayappiyo@gmail.com>
2026-05-13 20:55:29 +0530

    TF FILES CHANGED

--- terraform/backend_setup.tf
+++ terraform/backend_setup.tf
@@ -5,17 +5,3 @@ resource "aws_s3_bucket" "tf_state" {
     prevent_destroy = false
   }
 }
-
-resource "aws_dynamodb_table" "tf_state_lock" {
-  count = terraform.workspace == "default" ? 1 : 0
-  name  = "crms-tf-state-lock-dont-delete"
-  # AWS charges only when table is used.
-  billing_mode = "PAY_PER_REQUEST" # (or) PROVISIONED - But that requires fixed capacity setup.
-  # Primary key of DynamoDB table.
-  hash_key = "LockID" # Terraform uses this key to store lock information.
-  attribute {
-    # Defines database column.
-    name = "LockID" # Column name |  LockID is Stored as text/string
-    type = "S"      # S - String | N - Number | B - Binary
-  }
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,9 @@
 # Special Terraform block used to expose values after terraform apply.
 output "ec2_public_ip" {
-  value = length(module.compute) > 0 ? module.compute[0].instance_ip : null
+  value = length(module.compute) > 0 ? module.compute[0].ec2_public_ip : null
 }
 
 output "s3_bucket_name" {
   description = "Bucket Name of the Terraform State File Stored"
   value       = one(aws_s3_bucket.tf_state[*].bucket)
-}
-
-output "tf_state_lock_dynamoDB_name" {
-  description = "Dynamo DB Name of the Terraform State file Created"
-  value       = one(aws_dynamodb_table.tf_state_lock[*].name)
 }
