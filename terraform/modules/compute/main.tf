4e174fc79cbc12133e03cdfa929fb5a58d8f4bf5
shanjay-appiyo <shanjayappiyo@gmail.com>
2026-05-21 19:18:29 +0530

    Terraform test

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "_policy" {
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
